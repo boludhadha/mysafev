1c8e28d1b2a77eec36a31acdb8f4b096af772606
Bolu Dada <boluwatifedada78@gmail.com>
2025-05-20 00:34:38 +0100

    added speed and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
+
 
 
 export const metadata: Metadata = {
